278b5f9c86b3a6f0101ad88bb9ab2519e1b6f018
angryskull <angryskull@naver.com>
2020-01-09 23:31:31 +0900

    Revert "은노돌진구현버전"
    This reverts commit 740511faf247ff20882c953c86593c1d96c78477.

--- GameProject1/app/src/main/java/com/example/gameproject1/GameActivity.java
+++ GameProject1/app/src/main/java/com/example/gameproject1/GameActivity.java
@@ -5,16 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.os.Bundle;
-import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.TextView;
 
 import com.zerokol.views.joystickView.JoystickView;
 
 public class GameActivity extends AppCompatActivity{
-    private TextView mText;
-    private int timervalue = 3;
-    private TextView mUserCharacter;
     private TextView angleTextView;
     private TextView powerTextView;
     private TextView directionTextView;
@@ -39,25 +35,6 @@ public class GameActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-        mText=(TextView)findViewById(R.id.TextView1);
-        mUserCharacter=(TextView)findViewById(R.id.UserCharacter);
-
-        new CountDownTimer(4 * 1000, 1000){
-
-            @Override
-            public void onTick(long millisUntilFinished) { // 총 시간과 주기
-                mText.setText(String.valueOf(timervalue--));
-                if(timervalue == -1) {
-                    mText.setVisibility(View.INVISIBLE);
-                    mUserCharacter.setVisibility(View.VISIBLE);
-                }
-            }
-
-            @Override
-            public void onFinish() {
-
-            }
-        }.start();  // 타이머 시작
 //        angleTextView = (TextView) findViewById(R.id.angleTextView);
 //        powerTextView = (TextView) findViewById(R.id.powerTextView);
 //        directionTextView = (TextView) findViewById(R.id.directionTextView);
@@ -66,54 +43,39 @@ public class GameActivity extends AppCompatActivity{
         //Event listener that always returns the variation of the angle in degrees, motion power in percentage and direction of movement
         joystick.setOnJoystickMoveListener(new JoystickView.OnJoystickMoveListener() {
 
+
             @Override
             public void onValueChanged(int angle, int power, int direction) {
                 // TODO Auto-generated method stub
-    //                angleTextView.setText(" " + String.valueOf(angle) + "°");
-    //                powerTextView.setText(" " + String.valueOf(power) + "%");
+//                angleTextView.setText(" " + String.valueOf(angle) + "°");
+//                powerTextView.setText(" " + String.valueOf(power) + "%");
                 switch (direction) {
-                case JoystickView.FRONT:
-                    //directionTextView.setText(R.string.front_lab);
-                    mUserCharacter.setX(mUserCharacter.getX());
-                    mUserCharacter.setY(mUserCharacter.getY() - 10);
-                    break;
-                case JoystickView.FRONT_RIGHT:
-                    //directionTextView.setText(R.string.front_right_lab);
-                    mUserCharacter.setX(mUserCharacter.getX() - 8);
-                    mUserCharacter.setY(mUserCharacter.getY() - 8);
-                    break;
-                case JoystickView.RIGHT:
-                    //directionTextView.setText(R.string.right_lab);
-                    mUserCharacter.setX(mUserCharacter.getX() - 10);
-                    mUserCharacter.setY(mUserCharacter.getY());
-                    break;
-                case JoystickView.RIGHT_BOTTOM:
-                    //directionTextView.setText(R.string.right_bottom_lab);
-                    mUserCharacter.setX(mUserCharacter.getX() - 8);
-                    mUserCharacter.setY(mUserCharacter.getY() + 8);
-                    break;
-                case JoystickView.BOTTOM:
-                    //directionTextView.setText(R.string.bottom_lab);
-                    mUserCharacter.setX(mUserCharacter.getX());
-                    mUserCharacter.setY(mUserCharacter.getY() + 10);
-                    break;
-                case JoystickView.BOTTOM_LEFT:
-                    //directionTextView.setText(R.string.bottom_left_lab);
-                    mUserCharacter.setX(mUserCharacter.getX() + 8);
-                    mUserCharacter.setY(mUserCharacter.getY() + 8);
-                    break;
-                case JoystickView.LEFT:
-                    //directionTextView.setText(R.string.left_lab);
-                    mUserCharacter.setX(mUserCharacter.getX() + 10);
-                    mUserCharacter.setY(mUserCharacter.getY());
-                    break;
-                case JoystickView.LEFT_FRONT:
-                    //directionTextView.setText(R.string.left_front_lab);
-                    mUserCharacter.setX(mUserCharacter.getX() + 8);
-                    mUserCharacter.setY(mUserCharacter.getY() - 8);
-                    break;
-                default:
-                    //directionTextView.setText(R.string.center_lab);
+                    case JoystickView.FRONT:
+                        //directionTextView.setText(R.string.front_lab);
+                        break;
+                    case JoystickView.FRONT_RIGHT:
+                        //directionTextView.setText(R.string.front_right_lab);
+                        break;
+                    case JoystickView.RIGHT:
+                        //directionTextView.setText(R.string.right_lab);
+                        break;
+                    case JoystickView.RIGHT_BOTTOM:
+                        //directionTextView.setText(R.string.right_bottom_lab);
+                        break;
+                    case JoystickView.BOTTOM:
+                        //directionTextView.setText(R.string.bottom_lab);
+                        break;
+                    case JoystickView.BOTTOM_LEFT:
+                        //directionTextView.setText(R.string.bottom_left_lab);
+                        break;
+                    case JoystickView.LEFT:
+                        //directionTextView.setText(R.string.left_lab);
+                        break;
+                    case JoystickView.LEFT_FRONT:
+                        //directionTextView.setText(R.string.left_front_lab);
+                        break;
+                    default:
+                        //directionTextView.setText(R.string.center_lab);
                 }
             }
         }, JoystickView.DEFAULT_LOOP_INTERVAL);
